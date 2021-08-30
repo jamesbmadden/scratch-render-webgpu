112c86eb2a91d3102c4f8c28db86f237754e75d8
James Madden <jamesbmadden@gmail.com>
2021-08-30 13:41:54 -0700

    Add ghost effect (currently broken as alpha is not yet properly implemented)

--- src/shaders.wgsl.ts
+++ src/shaders.wgsl.ts
@@ -2,6 +2,7 @@ const shaderSource = `struct VertexOutput {
   [[builtin(position)]] pos: vec4<f32>;
   [[location(0)]] tex_coords: vec2<f32>;
   [[location(1)]] brightness: f32;
+  [[location(2)]] ghost: f32;
 };
 
 /*
@@ -52,6 +53,7 @@ fn vs_main(
   out.pos = vec4<f32>(apply_translate(apply_scale(pos, uniforms.data[1].x / 100.0, uniforms.data[1].y / 100.0), uniforms.data[0].y / 240.0, uniforms.data[0].z / 180.0), 1.0, 1.0);
   out.tex_coords = tex_coords;
   out.brightness = uniforms.data[2].w;
+  out.ghost = uniforms.data[3].x;
   return out;
 }
 
@@ -62,10 +64,14 @@ fn vs_main(
 fn apply_brightness(colour: vec4<f32>, brightness: f32) -> vec4<f32> {
   return vec4<f32>(clamp(colour.x + brightness, 0.0, 1.0), clamp(colour.y + brightness, 0.0, 1.0), clamp(colour.z + brightness, 0.0, 1.0), colour.w);
 }
+fn apply_ghost(colour: vec4<f32>, ghost: f32) -> vec4<f32> {
+  var alpha: f32 = 1.0 - clamp(ghost / 100.0, 0.0, 1.0);
+  return vec4<f32>(colour.xyz, alpha);
+}
 
 [[stage(fragment)]]
 fn fs_main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
-  return apply_brightness(textureSample(texture, tex_sampler, in.tex_coords), (in.brightness / 100.0));
+  return apply_ghost(apply_brightness(textureSample(texture, tex_sampler, in.tex_coords), (in.brightness / 100.0)), in.ghost);
 }`;
 
 export default shaderSource;
